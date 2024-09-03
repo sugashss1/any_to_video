62d6216aa41f5c8b6e0d58a7c960113909a00246
sugashss1 <sugashss1@gmail.com>
2024-09-03 15:37:02 +0530

    lossless video added

--- to image.py
+++ to image.py
@@ -8,24 +8,21 @@ def to_binary(file_path):
 def binary_to_colored_image(binary_data, output_image_path):
     """Convert binary data to a colored image."""
     # Calculate the image dimensions
-    height = 480
-    width = 640
+    height = 1080
+    width = 1920
     total_pixels=height*width
     data_len = len(binary_data)
     no_photos=math.ceil(data_len/(height*width*3))
-    #pixel_array=list(np.empty(shape=no_photos))
     # Create a numpy array from the binary data, padding if necessary
     padding = height*width*3*no_photos - data_len
     padded_data = binary_data + b'\0' * padding
     four = cv2.VideoWriter_fourcc(*"FFV1")
     fps = 25
-    out = cv2.VideoWriter('output.mkv',four, fps, (width, height))
+    out = cv2.VideoWriter(binary_image_path,four, fps, (width, height))
     for i in range(0,no_photos):
         pixel_array = np.frombuffer(padded_data[i*total_pixels*3:total_pixels*3+i*total_pixels*3], dtype=np.uint8).reshape((height, width, 3))
         # Create and save the image
-        img = Image.fromarray(pixel_array)
         out.write(pixel_array)
-        img.save("v/"+output_image_path+str(i)+".png")
         print(f"Image saved as {output_image_path+str(i)}")
     out.release()
 
