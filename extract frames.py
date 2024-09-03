40f344961a68ec5c52650d1c2c8bdd69f1aa0137
sugashss1 <sugashss1@gmail.com>
2024-09-03 22:08:45 +0530

    video to files seems to work

--- extract frames.py
+++ extract frames.py
@@ -1,17 +1,24 @@
 import cv2
-import os
+import numpy as np
+def video_to_binary(video_path,file_path):
+    cap = cv2.VideoCapture(video_path)
+    frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-def video_to_frames(video, path_output_dir):
-    # extract frames from a video and save to directory as 'x.png' where
-    # x is the frame index
-    vidcap = cv2.VideoCapture(video)
-    count = 0
-    while vidcap.isOpened():
-        success, image = vidcap.read()
-        if success:
-            cv2.imwrite(os.path.join(path_output_dir, '%d.png') % count, image)
-            count += 1
-        else:
-            break
-    cv2.destroyAllWindows()
-    vidcap.release()
+    array_of_image = np.empty((frameWidth,frameHeight, 3), np.dtype('uint8'))
+
+    fc = 0
+    ret = True
+    f = open(file_path, "wb")
+    f.write(array_of_image.tobytes())
+    while (fc < frameCount  and ret):
+        ret, array_of_image = cap.read()
+        fc += 1
+        f.write(array_of_image.tobytes())
+    cap.release()
+    cv2.waitKey(0)
+    f.close()
+binary_video_path = input("enter path to bin video:")
+file_path = input("give path of file:")
+video_to_binary(binary_video_path,file_path)
